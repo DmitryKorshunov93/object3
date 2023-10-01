3faddc330b08907c5dbbdbc3b0e4d301c89703cc
De93m <DmitryKorshunov93@gmail.com>
2023-10-01 15:22:12 +0300

    objects

--- src/homework/Book.java
+++ src/homework/Book.java
@@ -4,20 +4,36 @@ public class Book {
 
     private String bookTitle;
     private int age;
+    private Author author;
 
-    public Book(String bookTitle, int age) {
+
+    public Book(String bookTitle, int age, Author author) {
         this.bookTitle = bookTitle;
         this.age = age;
+        this.author = author;
     }
-    public String getBookTitle(){
+
+    public String getBookTitle() {
         return this.bookTitle;
     }
-    public Integer getAge(){
+
+    public Integer getAge() {
         return this.age;
     }
-    public void setAge (int age){
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
+    public String getFullName() {
+        return author.getName() + " " + author.getSurname();
+    }
 
+    public String toString() {
+        return getBookTitle() + " " + getAge() + " " + getFullName();
+    }
 }
